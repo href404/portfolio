42d60a7a5ea93b3a45e2bc485cf0e336f9202cc7
Pierre Julien <pierrejulienpro@gmail.com>
2021-07-02 14:41:39 +0200

    Création d'une page contact

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    [Route("contact")]
+    public class ContactController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
