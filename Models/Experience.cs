42233e927f112adffb9187a1b85f469e2d8197c1
Pierre Julien <pierrejulienpro@gmail.com>
2021-08-31 01:15:43 +0200

    Mise en forme de la page de détail d'une expérience

--- Models/Experience.cs
+++ Models/Experience.cs
@@ -3,6 +3,8 @@
     public class Experience : Article
     {
         public ExperienceType Type { get; init; }
+        public string Entity { get; set; }
+        public string Period { get; set; }
     }
     
     public enum ExperienceType { Company, School }
