b61d6702456fdc6bde36592425d5444898837cda
Pierre Julien <pierrejulienpro@gmail.com>
2021-08-31 23:32:19 +0200

    Rédaction de l'expérience "INTECH Sud"

--- ViewModels/Experience/ExperienceDetailViewModel.cs
+++ ViewModels/Experience/ExperienceDetailViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace Portfolio.ViewModels.Experience
+﻿using System;
+using Portfolio.Models;
+
+namespace Portfolio.ViewModels.Experience
 {
     public class ExperienceDetailViewModel : BaseDetailViewModel
     {
         public Models.Experience Experience { get; init; }
+
+        public string GetExperienceType()
+        {
+            return Experience.Name switch
+            {
+                "intech-sud" => "Étudiant",
+                "infomil-stage" => "Stagiaire",
+                "infomil-alternance" => "Apprenti Expert Logiciel",
+                _ => "Inconnu"
+            };
+        }
     }
 }
