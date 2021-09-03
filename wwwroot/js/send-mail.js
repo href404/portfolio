6f263347ec695c1c84a75bbaacfbad60428dc8ac
Pierre Julien <pierrejulienpro@gmail.com>
2021-09-03 13:06:42 +0200

    Mise en place d'une redirection vers un mailto lors de l'utilisation du formulaire de contact

--- wwwroot/js/send-mail.js
+++ wwwroot/js/send-mail.js
@@ -1,7 +1,10 @@
 ﻿function sendMail(e) {
     e.preventDefault();
     try {
-        alert("Votre email a été envoyé !")
+        const email = document.getElementById("email").value
+        const subject = document.getElementById("subject").value
+        const message = document.getElementById("message").value
+        window.location.href = `mailto:${email}?subject=${subject}&body=${message}`
     } catch (e) {
         throw new Error(e.message);
     }
