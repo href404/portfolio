ccd1f3b3706deb68c0d2aa4a4f7e7095460d8f53
Pierre Julien <pierrejulienpro@gmail.com>
2021-09-03 12:53:07 +0200

    Affichage d'une alert javascript lors de l'envoi du mail

---
+++ wwwroot/js/send-mail.js
@@ -0,0 +1,9 @@
+﻿function sendMail(e) {
+    e.preventDefault();
+    try {
+        alert("Votre email a été envoyé !")
+    } catch (e) {
+        throw new Error(e.message);
+    }
+    return false;
+}
