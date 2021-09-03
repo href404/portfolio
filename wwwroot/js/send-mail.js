e7e3c6be1e016290675d2c98559ab9b90739881e
Pierre Julien <pierrejulienpro@gmail.com>
2021-09-03 13:10:28 +0200

    Correction de la balise mailto et suppression du champ nom/prénom du formulaire de contact

--- wwwroot/js/send-mail.js
+++ wwwroot/js/send-mail.js
@@ -1,10 +1,9 @@
 ﻿function sendMail(e) {
     e.preventDefault();
     try {
-        const email = document.getElementById("email").value
         const subject = document.getElementById("subject").value
         const message = document.getElementById("message").value
-        window.location.href = `mailto:${email}?subject=${subject}&body=${message}`
+        window.location.href = `mailto:pierrejulienpro@outlook.fr?subject=${subject}&body=${message}`
     } catch (e) {
         throw new Error(e.message);
     }
